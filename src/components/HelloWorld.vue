780fdd9f2e0e4be1fc33a6c2bc0ad3dd2894de6d
Natalia <nflores.teoria@gmail.com>
2020-12-15 12:56:56 -0300

    Agrego signOut lo que elimina el problema con el caché a la hora de registarse

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,22 @@
 <template>
   <div class="hello container text-center mt-5">
-    <b-button size="lg" variant="outline-dark">Ir al Login</b-button>
+    <b-button size="lg" variant="outline-dark" @click="logout">Logout</b-button>
   </div>
 </template>
 
 <script>
+import firebase from 'firebase';
 export default {
   name: 'HelloWorld',
+  methods: {
+    logout() {
+      firebase.auth().signOut().then(() => {
+        this.$router.replace('/login');
+      }).catch((error) => {
+        console.error(error);
+      });
+    }
+  }
 }
 </script>
 
